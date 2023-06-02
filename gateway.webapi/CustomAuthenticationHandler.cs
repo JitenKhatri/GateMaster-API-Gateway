c355b4557c3bb6c751942d77687a9412b883b091
Jiten Khatri <jitenkhatri81@gmail.com>
2023-06-02 18:56:45 +0530

    Added authentication handler

---
+++ gateway.webapi/CustomAuthenticationHandler.cs
@@ -0,0 +1,39 @@
+﻿namespace gateway.webapi
+{
+    using Microsoft.AspNetCore.Authentication;
+    using Microsoft.Extensions.Logging;
+    using Microsoft.Extensions.Options;
+    using System;
+    using System.Security.Claims;
+    using System.Text.Encodings.Web;
+    using System.Threading.Tasks;
+
+    public class CustomAuthenticationHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        public CustomAuthenticationHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            ISystemClock clock)
+            : base(options, logger, encoder, clock)
+        {
+        }
+
+        protected override async Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var headerValue = Request.Headers["Authorization"].ToString();
+
+            if (headerValue != "Jiten123")
+            {
+                return AuthenticateResult.Fail("Invalid authorization header");
+            }
+            var claims = new[] { new Claim(ClaimTypes.Name, "username") };
+            var identity = new ClaimsIdentity(claims, Scheme.Name);
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, Scheme.Name);
+
+            return AuthenticateResult.Success(ticket);
+        }
+    }
+
+}
--- gateway.webapi/Program.cs
+++ gateway.webapi/Program.cs
@@ -1,3 +1,6 @@
+
+using gateway.webapi;
+using Microsoft.AspNetCore.Authentication;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
@@ -6,13 +9,18 @@ IConfiguration configuration = new ConfigurationBuilder()
                             .Build();
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddOcelot(configuration);
+builder.Services.AddAuthentication("TestKey")
+        .AddScheme<AuthenticationSchemeOptions, CustomAuthenticationHandler>("TestKey", options => { });
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 app.UseRouting();
+
 app.UseEndpoints(endpoints =>
 {
     endpoints.MapControllers();
 });
 app.UseOcelot();
+app.UseAuthentication();
 app.Run();
